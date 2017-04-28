13bbd2f180a91b44fdb5633cbd5df9f4c590627b
MouriNaruto <Mouri_Naruto@outlook.com>
2017-04-28 22:10:28 +0800

    **NSudo 4.4.1704.28** - 适配最新版M2-SDK - 适配最新版VC-LTL - 修改编译选项

--- NSudo/stdafx.h
+++ NSudo/stdafx.h
@@ -5,25 +5,27 @@
 
 #pragma once
 
+#define _CRT_SECURE_NO_WARNINGS
+#include <_msvcrt.h>
+
 // 为编译通过而禁用的警告
 #if _MSC_VER >= 1200
 // 编译器优化可能出现的警告（去除未引用函数并适当对一些函数使用内联）
+#pragma warning(disable:4464) // 相对包含路径包括 ".."
 #pragma warning(disable:4505) // 未引用的本地函数已移除(等级 4)
+#pragma warning(disable:4514) // 未引用的内联函数已移除(等级 4)
 #pragma warning(disable:4710) // 函数未内联(等级 4)
 #pragma warning(disable:4711) // 为自动内联扩展选定了函数(等级 1,只是提示信息)
 #endif
 
-#ifndef _DEBUG
-#define _CRT_SECURE_NO_WARNINGS
-#include <_msvcrt.h>
-#endif 
-
 #include "targetver.h"
 
+// M2-SDK
+#include <M2.SDK.h>
+
+// C++ 运行时库
 #include <stdio.h>
 #include <tchar.h>
-
-#include <M2.SDK.h>
 
 #include <commctrl.h>
 #pragma comment(lib, "comctl32.lib")
