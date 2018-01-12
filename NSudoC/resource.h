55e380dfdd94f96939086c261d71ba35cd3e7979
MouriNaruto <Mouri_Naruto@outlook.com>
2018-01-12 11:11:35 +0800

    **NSudo 6.0.1801.12** - 修复NSudoDuplicateSessionToken函数定义Bug（感谢 mingkuang） - 修复在图形界面下无法启用全部特权的Bug（感谢 abbodi1406 - 修复没有VC-LTL时x86和x86-64(AMD64)的Release编译配置未采用静态编译的Bug - 引入NSudoC和NSudoG

---
+++ NSudoC/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 NSudoC.rc 使用
+//
+#define NSUDO_VER_MINOR                 0
+#define NSUDO_VER_MAJOR                 6
+#define NSUDO_VER_REV                   11
+#define IDI_ICON1                       102
+#define IDI_NSUDO                       102
+#define NSUDO_VER_BUILD                 1801
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
