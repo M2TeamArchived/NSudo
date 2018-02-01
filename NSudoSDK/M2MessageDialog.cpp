1c3097fea5558f40c8a836be7707d0d0e6f1fe4b
MouriNaruto <Mouri_Naruto@outlook.com>
2018-02-01 13:34:27 +0800

    M2MessageDialog Update

--- NSudoSDK/M2MessageDialog.cpp
+++ NSudoSDK/M2MessageDialog.cpp
@@ -13,6 +13,76 @@ License: The MIT License
 #include "M2MessageDialog.h"
 #include "M2MessageDialogResource.h"
 
+// The parameter struct of the message dialog.
+struct DIALOG_BOX_PARAM
+{
+	HINSTANCE hInstance;
+	LPCWSTR lpIconName;
+	LPCWSTR lpTitle;
+	LPCWSTR lpContent;
+};
+
+// The callback function used of the message dialog.
+// Parameters:
+//   hwndDlg: A handle to the message dialog. 
+//   uMsg: The message. 
+//   wParam: Additional message-specific information. 
+//   lParam: Additional message-specific information. 
+// Return value:
+//   Typically, the dialog box procedure should return TRUE if it processed the
+//   message, and FALSE if it did not. If the dialog box procedure returns 
+//   FALSE, the dialog manager performs the default dialog operation in 
+//   response to the message.
+INT_PTR CALLBACK M2MessageDialogDialogCallBack(
+	_In_ HWND   hwndDlg,
+	_In_ UINT   uMsg,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+
+	if (WM_INITDIALOG == uMsg)
+	{
+		HICON hIcon = reinterpret_cast<HICON>(LoadImageW(
+			reinterpret_cast<DIALOG_BOX_PARAM*>(lParam)->hInstance,
+			reinterpret_cast<DIALOG_BOX_PARAM*>(lParam)->lpIconName,
+			IMAGE_ICON,
+			256,
+			256,
+			LR_SHARED));
+		if (nullptr != hIcon)
+		{
+			SendMessageW(
+				hwndDlg,
+				WM_SETICON,
+				ICON_SMALL,
+				reinterpret_cast<LPARAM>(hIcon));
+			SendMessageW(
+				hwndDlg,
+				WM_SETICON,
+				ICON_BIG,
+				reinterpret_cast<LPARAM>(hIcon));
+		}
+
+		SetWindowTextW(
+			hwndDlg,
+			reinterpret_cast<DIALOG_BOX_PARAM*>(lParam)->lpTitle);
+		SetWindowTextW(
+			GetDlgItem(hwndDlg, IDC_MESSAGE_DIALOG_EDIT),
+			reinterpret_cast<DIALOG_BOX_PARAM*>(lParam)->lpContent);
+
+		return (INT_PTR)TRUE;
+	}
+	else if (
+		(WM_CLOSE == uMsg) ||
+		(WM_COMMAND == uMsg && IDOK == LOWORD(wParam)))
+	{
+		EndDialog(hwndDlg, 0);
+	}
+
+	return FALSE;
+}
+
 // Creates and shows the message dialog.
 // Parameters:
 //   hInstance: A handle to the module which contains the message dialog 
@@ -36,73 +106,14 @@ License: The MIT License
 //   compatibility with previous versions of Windows. If the function fails for
 //   any other reason, the return value is –1. To get extended error 
 //   information, call GetLastError.
-INT_PTR M2MessageDialog(
+INT_PTR WINAPI M2MessageDialog(
 	_In_opt_ HINSTANCE hInstance,
 	_In_opt_ HWND hWndParent,
 	_In_opt_ LPCWSTR lpIconName,
 	_In_ LPCWSTR lpTitle,
 	_In_ LPCWSTR lpContent)
 {
-	struct DIALOG_BOX_PARAM
-	{
-		HINSTANCE hInstance;
-		LPCWSTR lpIconName;
-		LPCWSTR lpTitle;
-		LPCWSTR lpContent;
-	};
-
 	DIALOG_BOX_PARAM Param = { hInstance, lpIconName,lpTitle,lpContent };
-
-	auto DialogCallBack = [](
-		_In_ HWND hDlg,
-		_In_ UINT uMsg,
-		_In_ WPARAM wParam,
-		_In_ LPARAM lParam)
-	{
-		UNREFERENCED_PARAMETER(lParam);
-
-		if (WM_INITDIALOG == uMsg)
-		{
-			HICON hIcon = reinterpret_cast<HICON>(LoadImageW(
-				reinterpret_cast<DIALOG_BOX_PARAM*>(lParam)->hInstance,
-				reinterpret_cast<DIALOG_BOX_PARAM*>(lParam)->lpIconName,
-				IMAGE_ICON,
-				256,
-				256,
-				LR_SHARED));
-			if (nullptr != hIcon)
-			{
-				SendMessageW(
-					hDlg,
-					WM_SETICON,
-					ICON_SMALL,
-					reinterpret_cast<LPARAM>(hIcon));
-				SendMessageW(
-					hDlg,
-					WM_SETICON,
-					ICON_BIG,
-					reinterpret_cast<LPARAM>(hIcon));
-			}
-
-			SetWindowTextW(
-				hDlg,
-				reinterpret_cast<DIALOG_BOX_PARAM*>(lParam)->lpTitle);
-			SetWindowTextW(
-				GetDlgItem(hDlg, IDC_MESSAGE_DIALOG_EDIT),
-				reinterpret_cast<DIALOG_BOX_PARAM*>(lParam)->lpContent);
-
-			return (INT_PTR)TRUE;
-		}
-		else if (
-			(WM_CLOSE == uMsg) ||
-			(WM_COMMAND == uMsg && IDOK == LOWORD(wParam)))
-		{
-			EndDialog(hDlg, 0);
-		}
-
-		return FALSE;
-	};
-
 
 	M2EnablePerMonitorDialogScaling();
 
@@ -110,6 +121,6 @@ INT_PTR M2MessageDialog(
 		hInstance,
 		MAKEINTRESOURCEW(IDD_MESSAGE_DIALOG),
 		hWndParent,
-		DialogCallBack,
+		M2MessageDialogDialogCallBack,
 		reinterpret_cast<LPARAM>(&Param));
 }
