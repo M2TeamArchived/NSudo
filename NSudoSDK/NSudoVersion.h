d08e82286b56a62b17a9f308ef6ff595c7878e5f
MouriNaruto <Mouri_Naruto@outlook.com>
2018-04-05 17:33:19 +0800

    - Update Documents. - Hack RapidJSON to try to support Windows 10 ARM64.

--- NSudoSDK/NSudoVersion.h
+++ NSudoSDK/NSudoVersion.h
@@ -13,7 +13,7 @@ License: The MIT License
 #define NSUDO_VER_MAJOR 6
 #define NSUDO_VER_MINOR 0
 #define NSUDO_VER_BUILD 1804
-#define NSUDO_VER_REV 1
+#define NSUDO_VER_REV 5
 #endif
 
 #ifndef NSUDO_VER_FMT_COMMA
