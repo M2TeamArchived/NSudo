4b21e6b05893ecf1c9f2f8fa068be26314a2b894
MouriNaruto <Mouri_Naruto@outlook.com>
2018-07-23 01:37:29 +0800

    Add M2GetCurrentProcessModulePath and M2SpiltCommandLine functions to M2BaseHelpers. (Sync from project Nagisa.)

--- NSudoSDK/M2BaseHelpers.cpp
+++ NSudoSDK/M2BaseHelpers.cpp
@@ -183,4 +183,162 @@ HRESULT M2GetProcAddress(
 	return (nullptr != lpProcAddress) ? S_OK : M2GetLastError();
 }
 
+// Retrieves the path of the executable file of the current process.
+// Parameters:
+//   The function does not have parameters.
+// Return value:
+//   If the function succeeds, the return value is the path of the executable 
+//   file of the current process. If the function fails, the return value is an
+//   empty string.
+std::wstring M2GetCurrentProcessModulePath()
+{
+	std::wstring result(MAX_PATH, L'\0');
+	GetModuleFileNameW(nullptr, &result[0], (DWORD)(result.capacity()));
+	result.resize(wcslen(result.c_str()));
+	return result;
+}
+
+// Parses a command line string and returns an array of the command line 
+// arguments, along with a count of such arguments, in a way that is similar
+// to the standard C run-time.
+// Parameters:
+//   CommandLine: A string that contains the full command line. If this 
+//   parameter is an empty string the function returns an array with only 
+//   one empty string.
+// Return value:
+//   The return value is an array of the command line arguments, along with a 
+//   count of such arguments.
+std::vector<std::wstring> M2SpiltCommandLine(
+	const std::wstring& CommandLine)
+{
+	// Initialize the SplitArguments.
+	std::vector<std::wstring> SplitArguments;
+
+	wchar_t c = L'\0';
+	int copy_character;                   /* 1 = copy char to *args */
+	unsigned numslash;              /* num of backslashes seen */
+
+	std::wstring Buffer = L"";
+
+	/* first scan the program name, copy it, and count the bytes */
+	wchar_t* p = const_cast<wchar_t*>(CommandLine.c_str());
+
+	// A quoted program name is handled here. The handling is much
+	// simpler than for other arguments. Basically, whatever lies
+	// between the leading double-quote and next one, or a terminal null
+	// character is simply accepted. Fancier handling is not required
+	// because the program name must be a legal NTFS/HPFS file name.
+	// Note that the double-quote characters are not copied, nor do they
+	// contribute to character_count.
+	bool InQuotes = false;
+	do
+	{
+		if (*p == '"')
+		{
+			InQuotes = !InQuotes;
+			c = *p++;
+			continue;
+		}
+
+		// Copy character into argument:
+		Buffer.push_back(*p);
+
+		c = *p++;
+	} while (c != '\0' && (InQuotes || (c != ' ' && c != '\t')));
+
+	if (c == '\0')
+	{
+		p--;
+	}
+	else
+	{
+		Buffer.resize(Buffer.size() - 1);
+	}
+
+	// Save te argument.
+	SplitArguments.push_back(Buffer);
+
+	InQuotes = false;
+
+	// Loop on each argument
+	for (;;)
+	{
+		if (*p)
+		{
+			while (*p == ' ' || *p == '\t')
+				++p;
+		}
+
+		// End of arguments
+		if (*p == '\0')
+			break;
+
+		// Initialize the argument buffer.
+		Buffer = L"";
+
+		// Loop through scanning one argument:
+		for (;;)
+		{
+			copy_character = 1;
+
+			// Rules:
+			// 2N     backslashes   + " ==> N backslashes and begin/end quote
+			// 2N + 1 backslashes   + " ==> N backslashes + literal "
+			// N      backslashes       ==> N backslashes
+			numslash = 0;
+
+			while (*p == '\\')
+			{
+				// Count number of backslashes for use below
+				++p;
+				++numslash;
+			}
+
+			if (*p == '"')
+			{
+				// if 2N backslashes before, start/end quote, otherwise
+				// copy literally:
+				if (numslash % 2 == 0)
+				{
+					if (InQuotes && p[1] == '"')
+					{
+						p++; // Double quote inside quoted string
+					}
+					else
+					{
+						// Skip first quote char and copy second:
+						copy_character = 0; // Don't copy quote
+						InQuotes = !InQuotes;
+					}
+				}
+
+				numslash /= 2;
+			}
+
+			// Copy slashes:
+			while (numslash--)
+			{
+				Buffer.push_back(L'\\');
+			}
+
+			// If at end of arg, break loop:
+			if (*p == '\0' || (!InQuotes && (*p == ' ' || *p == '\t')))
+				break;
+
+			// Copy character into argument:
+			if (copy_character)
+			{
+				Buffer.push_back(*p);
+			}
+
+			++p;
+		}
+
+		// Save te argument.
+		SplitArguments.push_back(Buffer);
+	}
+
+	return SplitArguments;
+}
+
 #endif // _M2_BASE_HELPERS_
