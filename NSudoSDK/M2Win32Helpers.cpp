72acffa2d0e7a46374e1cfdc4363ef46320e8d86
MouriNaruto <Mouri_Naruto@outlook.com>
2018-09-27 22:16:20 +0800

    Add several implementations to M2Win32Helpers.

--- NSudoSDK/M2Win32Helpers.cpp
+++ NSudoSDK/M2Win32Helpers.cpp
@@ -10,6 +10,8 @@ License: The MIT License
 #include <Windows.h>
 #include <VersionHelpers.h>
 
+#include "M2BaseHelpers.h"
+
 #include "M2Win32Helpers.h"
 
 // Obtain the best matching resource with the specified type and name in the 
@@ -56,4 +58,245 @@ HRESULT M2LoadResource(
 	}
 
 	return __HRESULT_FROM_WIN32(GetLastError());
+}
+
+// Retrieves file system attributes for a specified file or directory.
+// Parameters:
+//   hFile: A handle to the file that contains the information to be retrieved.
+//   This handle should not be a pipe handle.
+// Return value:
+//   If the function succeeds, the return value contains the attributes of the 
+//   specified file or directory. For a list of attribute values and their 
+//   descriptions, see File Attribute Constants. If the function fails, the 
+//   return value is INVALID_FILE_ATTRIBUTES.To get extended error information,
+//   call GetLastError.
+DWORD M2GetFileAttributes(
+	_In_ HANDLE hFile)
+{
+	FILE_BASIC_INFO BasicInfo;
+
+	BOOL ret = GetFileInformationByHandleEx(
+		hFile,
+		FileBasicInfo,
+		&BasicInfo,
+		sizeof(FILE_BASIC_INFO));
+
+	return ret ? BasicInfo.FileAttributes : INVALID_FILE_ATTRIBUTES;
+}
+
+// Sets the attributes for a file or directory.
+// Parameters:
+//   hFile: A handle to the file for which to change information. This handle 
+//   must be opened with the appropriate permissions for the requested change. 
+//   For more information, see the Remarks and Example Code sections. This 
+//   handle should not be a pipe handle.
+//   dwFileAttributes: The file attributes to set for the file. This parameter
+//   can be one or more values, combined using the bitwise - OR operator. 
+//   However, all other values override FILE_ATTRIBUTE_NORMAL. For more 
+//   information, see the SetFileAttributes function.
+// Return value:
+//   The function will return HRESULT. If the function succeeds, the return 
+//   value is S_OK.
+HRESULT M2SetFileAttributes(
+	_In_ HANDLE hFile,
+	_In_ DWORD dwFileAttributes)
+{
+	FILE_BASIC_INFO BasicInfo = { 0 };
+	BasicInfo.FileAttributes =
+		dwFileAttributes & (
+			FILE_SHARE_READ |
+			FILE_SHARE_WRITE |
+			FILE_SHARE_DELETE |
+			FILE_ATTRIBUTE_ARCHIVE |
+			FILE_ATTRIBUTE_TEMPORARY |
+			FILE_ATTRIBUTE_OFFLINE |
+			FILE_ATTRIBUTE_NOT_CONTENT_INDEXED |
+			FILE_ATTRIBUTE_NO_SCRUB_DATA) |
+		FILE_ATTRIBUTE_NORMAL;
+
+	BOOL ret = SetFileInformationByHandle(
+		hFile,
+		FileBasicInfo,
+		&BasicInfo,
+		sizeof(FILE_BASIC_INFO));
+
+	return ret ? S_OK : M2GetLastError();
+}
+
+// Deletes an existing file.
+// Parameters:
+//   lpFileName: The name of the file to be deleted. In the ANSI version of 
+//   this function, the name is limited to MAX_PATH characters.To extend this
+//   limit to 32,767 wide characters, call the Unicode version of the function
+//   and prepend "\\?\" to the path. For more information, see Naming a File.
+//   bForceDeleteReadOnlyFile: If this parameter is true, the function removes 
+//   the read-only attribute first. 
+// Return value:
+//   The function will return HRESULT. If the function succeeds, the return 
+//   value is S_OK.
+HRESULT M2DeleteFile(
+	_In_ LPCWSTR lpFileName,
+	_In_ bool bForceDeleteReadOnlyFile)
+{
+	HRESULT hr = S_OK;
+
+	// Open the file.
+	HANDLE hFile = CreateFileW(
+		lpFileName,
+		SYNCHRONIZE | DELETE | FILE_READ_ATTRIBUTES | FILE_WRITE_ATTRIBUTES,
+		FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+		nullptr,
+		OPEN_EXISTING,
+		FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+		nullptr);
+	if (INVALID_HANDLE_VALUE == hFile)
+	{
+		hr = M2GetLastError();
+		goto Cleanup;
+	}
+
+	FILE_DISPOSITION_INFO DispostionInfo;
+	DispostionInfo.DeleteFile = TRUE;
+
+	DWORD OldAttribute = 0;
+
+	if (bForceDeleteReadOnlyFile)
+	{
+		// 保存旧属性值
+		OldAttribute = M2GetFileAttributes(hFile);
+
+		// 去除只读
+		M2SetFileAttributes(
+			hFile,
+			OldAttribute & (-1 ^ FILE_ATTRIBUTE_READONLY));
+	}
+
+	// Delete the file.
+	if (!SetFileInformationByHandle(
+		hFile,
+		FileDispositionInfo,
+		&DispostionInfo,
+		sizeof(FILE_DISPOSITION_INFO)))
+	{
+		hr = M2GetLastError();
+	}
+
+	// 如果删除未成功，则恢复原属性
+	if (bForceDeleteReadOnlyFile & !SUCCEEDED(hr))
+		M2SetFileAttributes(hFile, OldAttribute);
+
+Cleanup:
+	CloseHandle(hFile);
+
+	return hr;
+}
+
+// Retrieves the amount of space that is allocated for the file.
+// Parameters:
+//   lpFileName: The name of the file. In the ANSI version of this function, 
+//   the name is limited to MAX_PATH characters.To extend this limit to 32,767
+//   wide characters, call the Unicode version of the function and prepend 
+//   "\\?\" to the path. For more information, see Naming a File.
+//   lpAllocationSize: A pointer to a ULONGLONG value that receives the amount 
+//   of space that is allocated for the file, in bytes.
+// Return value:
+//   The function will return HRESULT. If the function succeeds, the return 
+//   value is S_OK.
+HRESULT M2GetFileAllocationSize(
+	_In_ LPCWSTR lpFileName,
+	_Out_ PULONGLONG lpAllocationSize)
+{
+	*lpAllocationSize = 0;
+
+	HRESULT hr = S_OK;
+
+	// Open the file.
+	HANDLE hFile = CreateFileW(
+		lpFileName,
+		GENERIC_READ | SYNCHRONIZE,
+		FILE_SHARE_READ,
+		nullptr,
+		OPEN_EXISTING,
+		FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+		nullptr);
+	if (INVALID_HANDLE_VALUE == hFile)
+	{
+		hr = M2GetLastError();
+		goto Cleanup;
+	}
+
+	FILE_STANDARD_INFO StandardInfo;
+
+	if (!GetFileInformationByHandleEx(
+		hFile,
+		FileStandardInfo,
+		&StandardInfo,
+		sizeof(FILE_STANDARD_INFO)))
+	{
+		hr = M2GetLastError();
+		goto Cleanup;
+	}
+
+	*lpAllocationSize = static_cast<ULONGLONG>(
+		StandardInfo.AllocationSize.QuadPart);
+
+Cleanup:
+	CloseHandle(hFile);
+
+	return hr;
+}
+
+// Retrieves the size of the specified file.
+// Parameters:
+//   lpFileName: The name of the file. In the ANSI version of this function, 
+//   the name is limited to MAX_PATH characters.To extend this limit to 32,767
+//   wide characters, call the Unicode version of the function and prepend 
+//   "\\?\" to the path. For more information, see Naming a File.
+//   lpFileSize: A pointer to a ULONGLONG value that receives the file size, in
+//   bytes.
+// Return value:
+//   The function will return HRESULT. If the function succeeds, the return 
+//   value is S_OK.
+HRESULT M2GetFileSize(
+	_In_ LPCWSTR lpFileName,
+	_Out_ PULONGLONG lpFileSize)
+{
+	*lpFileSize = 0;
+
+	HRESULT hr = S_OK;
+
+	// Open the file.
+	HANDLE hFile = CreateFileW(
+		lpFileName,
+		GENERIC_READ | SYNCHRONIZE,
+		FILE_SHARE_READ,
+		nullptr,
+		OPEN_EXISTING,
+		FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+		nullptr);
+	if (INVALID_HANDLE_VALUE == hFile)
+	{
+		hr = M2GetLastError();
+		goto Cleanup;
+	}
+
+	FILE_STANDARD_INFO StandardInfo;
+
+	if (!GetFileInformationByHandleEx(
+		hFile,
+		FileStandardInfo,
+		&StandardInfo,
+		sizeof(FILE_STANDARD_INFO)))
+	{
+		hr = M2GetLastError();
+		goto Cleanup;
+	}
+
+	*lpFileSize = static_cast<ULONGLONG>(
+		StandardInfo.EndOfFile.QuadPart);
+
+Cleanup:
+	CloseHandle(hFile);
+
+	return hr;
 }
--- NSudoSDK/M2Win32Helpers.h
+++ NSudoSDK/M2Win32Helpers.h
@@ -41,4 +41,79 @@ HRESULT M2LoadResource(
 	_In_ LPCWSTR lpType,
 	_In_ LPCWSTR lpName);
 
+// Retrieves file system attributes for a specified file or directory.
+// Parameters:
+//   hFile: A handle to the file that contains the information to be retrieved.
+//   This handle should not be a pipe handle.
+// Return value:
+//   If the function succeeds, the return value contains the attributes of the 
+//   specified file or directory. For a list of attribute values and their 
+//   descriptions, see File Attribute Constants. If the function fails, the 
+//   return value is INVALID_FILE_ATTRIBUTES.To get extended error information,
+//   call GetLastError.
+DWORD M2GetFileAttributes(
+	_In_ HANDLE hFile);
+
+// Sets the attributes for a file or directory.
+// Parameters:
+//   hFile: A handle to the file for which to change information. This handle 
+//   must be opened with the appropriate permissions for the requested change. 
+//   For more information, see the Remarks and Example Code sections. This 
+//   handle should not be a pipe handle.
+//   dwFileAttributes: The file attributes to set for the file. This parameter
+//   can be one or more values, combined using the bitwise - OR operator. 
+//   However, all other values override FILE_ATTRIBUTE_NORMAL. For more 
+//   information, see the SetFileAttributes function.
+// Return value:
+//   The function will return HRESULT. If the function succeeds, the return 
+//   value is S_OK.
+HRESULT M2SetFileAttributes(
+	_In_ HANDLE hFile,
+	_In_ DWORD dwFileAttributes);
+
+// Deletes an existing file.
+// Parameters:
+//   lpFileName: The name of the file to be deleted. In the ANSI version of 
+//   this function, the name is limited to MAX_PATH characters.To extend this
+//   limit to 32,767 wide characters, call the Unicode version of the function
+//   and prepend "\\?\" to the path. For more information, see Naming a File.
+//   bForceDeleteReadOnlyFile: If this parameter is true, the function removes 
+//   the read-only attribute first. 
+// Return value:
+//   The function will return HRESULT. If the function succeeds, the return 
+//   value is S_OK.
+HRESULT M2DeleteFile(
+	_In_ LPCWSTR lpFileName,
+	_In_ bool bForceDeleteReadOnlyFile);
+
+// Retrieves the amount of space that is allocated for the file.
+// Parameters:
+//   lpFileName: The name of the file. In the ANSI version of this function, 
+//   the name is limited to MAX_PATH characters.To extend this limit to 32,767
+//   wide characters, call the Unicode version of the function and prepend 
+//   "\\?\" to the path. For more information, see Naming a File.
+//   lpAllocationSize: A pointer to a ULONGLONG value that receives the amount 
+//   of space that is allocated for the file, in bytes.
+// Return value:
+//   The function will return HRESULT. If the function succeeds, the return 
+//   value is S_OK.
+HRESULT M2GetFileAllocationSize(
+	_In_ LPCWSTR lpFileName,
+	_Out_ PULONGLONG lpAllocationSize);
+
+// Retrieves the size of the specified file.
+// Parameters:
+//   lpFileName: The name of the file. In the ANSI version of this function, 
+//   the name is limited to MAX_PATH characters.To extend this limit to 32,767
+//   wide characters, call the Unicode version of the function and prepend 
+//   "\\?\" to the path. For more information, see Naming a File.
+//   lpFileSize: A pointer to a ULONGLONG value that receives the file size, in
+//   bytes.
+// Return value:
+//   The function will return HRESULT. If the function succeeds, the return 
+//   value is S_OK.
+HRESULT M2GetFileSize(
+	_In_ LPCWSTR lpFileName,
+	_Out_ PULONGLONG lpFileSize);
+
 #endif // _M2_WIN32_HELPERS_
