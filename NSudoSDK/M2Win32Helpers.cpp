466becff6f302dd9eb299024d9a35988365beb27
MouriNaruto <Mouri_Naruto@outlook.com>
2019-04-15 22:33:44 +0800

    Update M2Win32Helpers.

--- NSudoSDK/M2Win32Helpers.cpp
+++ NSudoSDK/M2Win32Helpers.cpp
@@ -8,8 +8,6 @@
  * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
  */
 
-#include "stdafx.h"
-
 #include <Windows.h>
 #include <VersionHelpers.h>
 
@@ -130,6 +128,8 @@ HRESULT M2StartService(
     _In_ LPCWSTR lpServiceName,
     _Out_ LPSERVICE_STATUS_PROCESS lpServiceStatus)
 {
+    memset(lpServiceStatus, 0, sizeof(LPSERVICE_STATUS_PROCESS));
+
     M2::CServiceHandle hSCM;
     M2::CServiceHandle hService;
 
@@ -240,13 +240,13 @@ HRESULT M2LoadLibraryEx(
         if ((Flags & LOAD_LIBRARY_SEARCH_SYSTEM32) &&
             (M2GetLastErrorKnownFailedCall() == ERROR_INVALID_PARAMETER))
         {
-            if (!wcschr(LibraryFileName, (wchar_t)'\\'))
+            if (!wcschr(LibraryFileName, L'\\'))
             {
                 UINT NewLength = GetSystemDirectoryW(
                     Buffer,
                     BufferLength);
 
-                Buffer[NewLength++] = '\\';
+                Buffer[NewLength++] = L'\\';
 
                 for (; *LibraryFileName; ++LibraryFileName)
                 {
@@ -263,4 +263,203 @@ HRESULT M2LoadLibraryEx(
     }
 
     return ModuleHandle ? S_OK : M2GetLastHRESULTErrorKnownFailedCall();
+}
+
+/**
+ * Creates the specified registry key. If the key already exists, the function
+ * opens it. Note that key names are not case sensitive.
+ *
+ * @param hKey A handle to an open registry key.
+ * @param lpSubKey The name of a subkey that this function opens or creates
+ * @param Reserved This parameter is reserved and must be zero.
+ * @param lpClass The user-defined class type of this key.
+ * @param dwOptions This parameter can be one of the following values:
+ *                  REG_OPTION_BACKUP_RESTORE, REG_OPTION_CREATE_LINK,
+ *                  REG_OPTION_NON_VOLATILE, REG_OPTION_VOLATILE.
+ * @param samDesired A mask that specifies the access rights for the key to be
+ *                   created.
+ * @param lpSecurityAttributes A pointer to a SECURITY_ATTRIBUTES structure
+ *                             that determines whether the returned handle can
+ *                             be inherited by child processes.
+ * @param phkResult A pointer to a variable that receives a handle to the
+ *                  opened or created key.
+ * @param lpdwDisposition A pointer to a variable that receives one of the
+ *                        following disposition values.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark For more information, see RegCreateKeyEx.
+ */
+HRESULT M2RegCreateKey(
+    _In_ HKEY hKey,
+    _In_ LPCWSTR lpSubKey,
+    _Reserved_ DWORD Reserved,
+    _In_opt_ LPWSTR lpClass,
+    _In_ DWORD dwOptions,
+    _In_ REGSAM samDesired,
+    _In_opt_ CONST LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    _Out_ PHKEY phkResult,
+    _Out_opt_ LPDWORD lpdwDisposition)
+{
+    return HRESULT_FROM_WIN32(RegCreateKeyExW(
+        hKey,
+        lpSubKey,
+        Reserved,
+        lpClass,
+        dwOptions,
+        samDesired,
+        lpSecurityAttributes,
+        phkResult,
+        lpdwDisposition));
+}
+
+/**
+ * Retrieves the type and data for the specified value name associated with an
+ * open registry key.
+ *
+ * @param hKey A handle to an open registry key.
+ * @param lpValueName The name of the registry value.
+ * @param lpReserved This parameter is reserved and must be NULL.
+ * @param lpType A pointer to a variable that receives a code indicating the
+ *              type of data stored in the specified value.
+ * @param lpData A pointer to a buffer that receives the value's data.
+ * @param lpcbData A pointer to a variable that specifies the size of the
+ *                 buffer pointed to by the lpData parameter, in bytes.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark For more information, see RegQueryValueEx.
+ */
+HRESULT M2RegQueryValue(
+    _In_ HKEY hKey,
+    _In_opt_ LPCWSTR lpValueName,
+    _Reserved_ LPDWORD lpReserved,
+    _Out_opt_ LPDWORD lpType,
+    _Out_opt_ LPBYTE lpData,
+    _Inout_opt_ LPDWORD lpcbData)
+{
+    return HRESULT_FROM_WIN32(RegQueryValueExW(
+        hKey,
+        lpValueName,
+        lpReserved,
+        lpType,
+        lpData,
+        lpcbData));
+}
+
+/**
+ * Retrieves the type and data for the specified value name associated with an
+ * open registry key.
+ *
+ * @param hKey A handle to an open registry key.
+ * @param lpValueName The name of the value to be set.
+ * @param Reserved This parameter is reserved and must be zero.
+ * @param dwType The type of data pointed to by the lpData parameter.
+ * @param lpData The data to be stored.
+ * @param cbData The size of the information pointed to by the lpData
+ *               parameter, in bytes.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark For more information, see RegSetValueEx.
+ */
+HRESULT M2RegSetValue(
+    _In_ HKEY hKey,
+    _In_opt_ LPCWSTR lpValueName,
+    _Reserved_ DWORD Reserved,
+    _In_ DWORD dwType,
+    _In_opt_ CONST BYTE* lpData,
+    _In_ DWORD cbData)
+{
+    return HRESULT_FROM_WIN32(RegSetValueExW(
+        hKey,
+        lpValueName,
+        Reserved,
+        dwType,
+        lpData,
+        cbData));
+}
+
+/**
+ * Creates a single uninitialized object of the class associated with a
+ * specified CLSID.
+ *
+ * @param lpszCLSID The string representation of the CLSID.
+ * @param pUnkOuter If NULL, indicates that the object is not being created as
+ *                  part of an aggregate. If non-NULL, pointer to the aggregate
+ *                  object's IUnknown interface (the controlling IUnknown).
+ * @param dwClsContext Context in which the code that manages the newly created
+ *                     object will run. The values are taken from the
+ *                     enumeration CLSCTX.
+ * @param lpszIID A pointer to the string representation of the IID.
+ * @param ppv Address of pointer variable that receives the interface pointer
+ *            requested in riid. Upon successful return, *ppv contains the
+ *            requested interface pointer. Upon failure, *ppv contains NULL.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark For more information, see CoCreateInstance.
+ */
+HRESULT M2CoCreateInstance(
+    _In_ LPCWSTR lpszCLSID,
+    _In_opt_ LPUNKNOWN pUnkOuter,
+    _In_ DWORD dwClsContext,
+    _In_ LPCWSTR lpszIID,
+    _Out_ LPVOID* ppv)
+{
+    CLSID clsid;
+    IID iid;
+
+    HRESULT hr = CLSIDFromString(lpszCLSID, &clsid);
+    if (SUCCEEDED(hr))
+    {
+        hr = IIDFromString(lpszIID, &iid);
+        if (SUCCEEDED(hr))
+        {
+            hr = CoCreateInstance(clsid, pUnkOuter, dwClsContext, iid, ppv);
+        }
+    }
+
+    return hr;
+}
+
+/**
+ * Determines whether the interface id have the correct interface name.
+ *
+ * @param InterfaceID A pointer to the string representation of the IID.
+ * @param InterfaceName A pointer to the interface name string.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ */
+HRESULT M2CoCheckInterfaceName(
+    _In_ LPCWSTR InterfaceID,
+    _In_ LPCWSTR InterfaceName)
+{
+    HKEY hKey = nullptr;
+    HRESULT hr = M2RegCreateKey(
+        HKEY_CLASSES_ROOT,
+        (std::wstring(L"Interface\\") + InterfaceID).c_str(),
+        0,
+        nullptr,
+        0,
+        KEY_READ,
+        nullptr,
+        &hKey,
+        nullptr);
+    if (SUCCEEDED(hr))
+    {
+        DWORD Type = 0;
+        wchar_t Data[256] = { 0 };
+        DWORD cbData = 256;
+
+        hr = M2RegQueryValue(
+            hKey,
+            nullptr,
+            nullptr,
+            &Type,
+            reinterpret_cast<LPBYTE>(Data),
+            &cbData);
+        if (SUCCEEDED(hr))
+        {
+            if (0 != _wcsicmp(Data, InterfaceName))
+            {
+                hr = E_NOINTERFACE;
+            }
+        }
+
+        RegCloseKey(hKey);
+    }
+
+    return hr;
 }
--- NSudoSDK/M2Win32Helpers.h
+++ NSudoSDK/M2Win32Helpers.h
@@ -198,4 +198,119 @@ HRESULT M2LoadLibraryEx(
     _In_ LPCWSTR LibraryFileName,
     _In_ DWORD Flags);
 
+/**
+ * Creates the specified registry key. If the key already exists, the function
+ * opens it. Note that key names are not case sensitive.
+ *
+ * @param hKey A handle to an open registry key.
+ * @param lpSubKey The name of a subkey that this function opens or creates
+ * @param Reserved This parameter is reserved and must be zero.
+ * @param lpClass The user-defined class type of this key.
+ * @param dwOptions This parameter can be one of the following values:
+ *                  REG_OPTION_BACKUP_RESTORE, REG_OPTION_CREATE_LINK,
+ *                  REG_OPTION_NON_VOLATILE, REG_OPTION_VOLATILE.
+ * @param samDesired A mask that specifies the access rights for the key to be
+ *                   created.
+ * @param lpSecurityAttributes A pointer to a SECURITY_ATTRIBUTES structure
+ *                             that determines whether the returned handle can
+ *                             be inherited by child processes.
+ * @param phkResult A pointer to a variable that receives a handle to the
+ *                  opened or created key.
+ * @param lpdwDisposition A pointer to a variable that receives one of the
+ *                        following disposition values.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark For more information, see RegCreateKeyEx.
+ */
+HRESULT M2RegCreateKey(
+    _In_ HKEY hKey,
+    _In_ LPCWSTR lpSubKey,
+    _Reserved_ DWORD Reserved,
+    _In_opt_ LPWSTR lpClass,
+    _In_ DWORD dwOptions,
+    _In_ REGSAM samDesired,
+    _In_opt_ CONST LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    _Out_ PHKEY phkResult,
+    _Out_opt_ LPDWORD lpdwDisposition);
+
+/**
+ * Retrieves the type and data for the specified value name associated with an
+ * open registry key.
+ *
+ * @param hKey A handle to an open registry key.
+ * @param lpValueName The name of the registry value.
+ * @param lpReserved This parameter is reserved and must be NULL.
+ * @param lpType A pointer to a variable that receives a code indicating the
+ *              type of data stored in the specified value.
+ * @param lpData A pointer to a buffer that receives the value's data.
+ * @param lpcbData A pointer to a variable that specifies the size of the
+ *                 buffer pointed to by the lpData parameter, in bytes.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark For more information, see RegQueryValueEx.
+ */
+HRESULT M2RegQueryValue(
+    _In_ HKEY hKey,
+    _In_opt_ LPCWSTR lpValueName,
+    _Reserved_ LPDWORD lpReserved,
+    _Out_opt_ LPDWORD lpType,
+    _Out_opt_ LPBYTE lpData,
+    _Inout_opt_ LPDWORD lpcbData);
+
+/**
+ * Retrieves the type and data for the specified value name associated with an
+ * open registry key.
+ *
+ * @param hKey A handle to an open registry key.
+ * @param lpValueName The name of the value to be set.
+ * @param Reserved This parameter is reserved and must be zero.
+ * @param dwType The type of data pointed to by the lpData parameter.
+ * @param lpData The data to be stored.
+ * @param cbData The size of the information pointed to by the lpData
+ *               parameter, in bytes.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark For more information, see RegSetValueEx.
+ */
+HRESULT M2RegSetValue(
+    _In_ HKEY hKey,
+    _In_opt_ LPCWSTR lpValueName,
+    _Reserved_ DWORD Reserved,
+    _In_ DWORD dwType,
+    _In_opt_ CONST BYTE* lpData,
+    _In_ DWORD cbData);
+
+/**
+ * Creates a single uninitialized object of the class associated with a
+ * specified CLSID.
+ *
+ * @param lpszCLSID The string representation of the CLSID.
+ * @param pUnkOuter If NULL, indicates that the object is not being created as
+ *                  part of an aggregate. If non-NULL, pointer to the aggregate
+ *                  object's IUnknown interface (the controlling IUnknown).
+ * @param dwClsContext Context in which the code that manages the newly created
+ *                     object will run. The values are taken from the
+ *                     enumeration CLSCTX.
+ * @param lpszIID A pointer to the string representation of the IID.
+ * @param ppv Address of pointer variable that receives the interface pointer
+ *            requested in riid. Upon successful return, *ppv contains the
+ *            requested interface pointer. Upon failure, *ppv contains NULL.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark For more information, see CoCreateInstance.
+ */
+HRESULT M2CoCreateInstance(
+    _In_ LPCWSTR lpszCLSID,
+    _In_opt_ LPUNKNOWN pUnkOuter,
+    _In_ DWORD dwClsContext,
+    _In_ LPCWSTR lpszIID,
+    _Out_ LPVOID* ppv);
+
+/**
+ * Determines whether the interface id have the correct interface name.
+ *
+ * @param InterfaceID A pointer to the string representation of the IID.
+ * @param InterfaceName A pointer to the interface name string.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ */
+HRESULT M2CoCheckInterfaceName(
+    _In_ LPCWSTR InterfaceID,
+    _In_ LPCWSTR InterfaceName);
+
 #endif // _M2_WIN32_HELPERS_
