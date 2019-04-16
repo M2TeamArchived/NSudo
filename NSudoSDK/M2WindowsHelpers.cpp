9b944e62af0a9dc1e2bbf2f4ca8641783206ed2c
MouriNaruto <Mouri_Naruto@outlook.com>
2019-04-16 23:25:34 +0800

    Add M2WindowsHelpers.

---
+++ NSudoSDK/M2WindowsHelpers.cpp
@@ -0,0 +1,13 @@
+﻿/*
+ * PROJECT:   M2-Team Common Library
+ * FILE:      M2WindowsHelpers.cpp
+ * PURPOSE:   Implementation for the Windows helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "stdafx.h"
+
+#include "M2WindowsHelpers.h"
