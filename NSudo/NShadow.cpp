c5f2c20c89f2586f0cb049ac784602a06d13e955
MouriNaruto <Mouri_Naruto@outlook.com>
2019-05-16 02:22:16 +0800

    Add NShadow.

---
+++ NSudo/NShadow.cpp
@@ -0,0 +1,23 @@
+﻿/*
+ * PROJECT:   NShadow
+ * FILE:      NShadow.cpp
+ * PURPOSE:   Implementation for NShadow
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "stdafx.h"
+
+#include "NShadow.h"
+
+typedef struct _NSHADOW_ENVIRONMENT_CONFIGURATION
+{
+    std::wstring CurrentDirectory;
+    std::vector<std::wstring> EnvironmentVariables;
+
+    std::wstring TokenIntegrityLevel;
+    std::vector<std::wstring> TokenPrivileges;
+
+} NSHADOW_ENVIRONMENT_CONFIGURATION, *PNSHADOW_ENVIRONMENT_CONFIGURATION;
---
+++ NSudo/NShadow.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   NShadow
+ * FILE:      NShadow.h
+ * PURPOSE:   Definition for NShadow
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#pragma once
+
+#ifndef NSHADOW
+#define NSHADOW
+
+#endif // !NSHADOW
