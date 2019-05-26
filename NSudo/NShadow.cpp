a226f1b9d281e849b8043d72c5b67f32442cc19a
MouriNaruto <Mouri_Naruto@outlook.com>
2019-05-27 01:19:34 +0800

    Note something in NShadow.

--- NSudo/NShadow.cpp
+++ NSudo/NShadow.cpp
@@ -21,3 +21,45 @@ typedef struct _NSHADOW_ENVIRONMENT_CONFIGURATION
     std::vector<std::wstring> TokenPrivileges;
 
 } NSHADOW_ENVIRONMENT_CONFIGURATION, *PNSHADOW_ENVIRONMENT_CONFIGURATION;
+
+
+void x(
+    _In_opt_ LPCWSTR lpApplicationName,
+    _Inout_opt_ LPWSTR lpCommandLine,
+
+    _In_ DWORD dwCreationFlags,
+    _In_opt_ LPVOID lpEnvironment,
+    _In_opt_ LPCWSTR lpCurrentDirectory,
+    _In_ LPSTARTUPINFOW lpStartupInfo,
+    _Out_ LPPROCESS_INFORMATION lpProcessInformation,
+
+
+
+
+    _In_opt_ HANDLE hToken,
+    
+    _In_opt_ LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    _In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    _In_ BOOL bInheritHandles,
+    
+
+    //_In_opt_ LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    //_In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    //_In_ BOOL bInheritHandles,
+
+
+    _In_        LPCWSTR lpUsername,
+    _In_opt_    LPCWSTR lpDomain,
+    _In_        LPCWSTR lpPassword,
+    _In_        DWORD dwLogonFlags
+
+    //_In_        HANDLE hToken,
+    //_In_        DWORD dwLogonFlags
+)
+{
+    CreateProcessAsUserW;
+    CreateProcessW;
+
+    CreateProcessWithLogonW;
+    CreateProcessWithTokenW;
+}
