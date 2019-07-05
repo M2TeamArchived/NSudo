a7acf2cbd9336da1a4a8b6d91c4179b99b1b2c86
MouriNaruto <Mouri_Naruto@outlook.com>
2019-07-06 03:36:11 +0800

    Add M2WindowsDownlevelHelpers.

---
+++ NSudoSDK/M2WindowsDownlevelHelpers.cpp
@@ -0,0 +1,19 @@
+﻿/*
+ * PROJECT:   M2-Team Common Library
+ * FILE:      M2WindowsDownlevelHelpers.cpp
+ * PURPOSE:   Implementation for the Windows downlevel helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "stdafx.h"
+
+#include "M2WindowsDownlevelHelpers.h"
+
+#ifdef _M2_WINDOWS_DOWNLEVEL_HELPERS_
+
+
+
+#endif // _M2_WINDOWS_DOWNLEVEL_HELPERS_
---
+++ NSudoSDK/M2WindowsDownlevelHelpers.h
@@ -0,0 +1,20 @@
+﻿/*
+ * PROJECT:   M2-Team Common Library
+ * FILE:      M2WindowsDownlevelHelpers.h
+ * PURPOSE:   Definition for the Windows downlevel helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#pragma once
+
+#ifndef _M2_WINDOWS_DOWNLEVEL_HELPERS_
+#define _M2_WINDOWS_DOWNLEVEL_HELPERS_
+
+#include <Windows.h>
+
+
+
+#endif // !_M2_WINDOWS_DOWNLEVEL_HELPERS_
