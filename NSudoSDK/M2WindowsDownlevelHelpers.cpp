873377e77c639c8aa265bfecd3b788139af2d9bf
MouriNaruto <Mouri_Naruto@outlook.com>
2019-07-06 11:39:11 +0800

    Move some implementations to M2WindowsDownlevelHelpers.

--- NSudoSDK/M2WindowsDownlevelHelpers.cpp
+++ NSudoSDK/M2WindowsDownlevelHelpers.cpp
@@ -14,6 +14,345 @@
 
 #ifdef _M2_WINDOWS_DOWNLEVEL_HELPERS_
 
+#include <assert.h>
+#include <process.h>
 
+DWORD M2GetLastWin32Error(
+    _In_ BOOL IsLastFunctionCallSucceeded,
+    _In_ BOOL UseLastErrorWhenSucceeded)
+{
+    if (IsLastFunctionCallSucceeded && !UseLastErrorWhenSucceeded)
+        return ERROR_SUCCESS;
+
+    DWORD LastError = GetLastError();
+
+    if (!IsLastFunctionCallSucceeded && ERROR_SUCCESS == LastError)
+        return ERROR_FUNCTION_FAILED;
+
+    return LastError;
+}
+
+HRESULT M2GetLastHResultError(
+    _In_ BOOL IsLastFunctionCallSucceeded,
+    _In_ BOOL UseLastErrorWhenSucceeded)
+{
+    return HRESULT_FROM_WIN32(M2GetLastWin32Error(
+        IsLastFunctionCallSucceeded,
+        UseLastErrorWhenSucceeded));
+}
+
+HRESULT M2HeapAlloc(
+    _Out_ PVOID* lpNewMem,
+    _In_ HANDLE hHeap,
+    _In_ DWORD dwFlags,
+    _In_ SIZE_T dwBytes)
+{
+    *lpNewMem = HeapAlloc(hHeap, dwFlags, dwBytes);
+    return *lpNewMem ? S_OK : __HRESULT_FROM_WIN32(ERROR_NOT_ENOUGH_MEMORY);
+}
+
+HRESULT M2HeapReAlloc(
+    _Out_ PVOID* lpNewMem,
+    _Inout_ HANDLE hHeap,
+    _In_ DWORD dwFlags,
+    _In_ LPVOID lpMem,
+    _In_ SIZE_T dwBytes)
+{
+    *lpNewMem = HeapReAlloc(hHeap, dwFlags, lpMem, dwBytes);
+    return *lpNewMem ? S_OK : __HRESULT_FROM_WIN32(ERROR_NOT_ENOUGH_MEMORY);
+}
+
+HRESULT M2HeapFree(
+    _Inout_ HANDLE hHeap,
+    _In_ DWORD dwFlags,
+    _In_ LPVOID lpMem)
+{
+    return M2GetLastHResultError(HeapFree(hHeap, dwFlags, lpMem));
+}
+
+HRESULT M2CloseHandle(
+    _In_ HANDLE hObject)
+{
+    return M2GetLastHResultError(CloseHandle(hObject));
+}
+
+HRESULT M2CreateThread(
+    _Out_ PHANDLE lpThreadHandle,
+    _In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    _In_ SIZE_T dwStackSize,
+    _In_ LPTHREAD_START_ROUTINE lpStartAddress,
+    _In_opt_ LPVOID lpParameter,
+    _In_ DWORD dwCreationFlags,
+    _Out_opt_ LPDWORD lpThreadId)
+{
+    // sanity check for lpThreadId
+    assert(sizeof(DWORD) == sizeof(unsigned));
+
+    typedef unsigned(__stdcall * routine_type)(void*);
+
+    // _beginthreadex calls CreateThread which will set the last error
+    // value before it returns.
+    *lpThreadHandle = reinterpret_cast<HANDLE>(_beginthreadex(
+        lpThreadAttributes,
+        static_cast<unsigned>(dwStackSize),
+        reinterpret_cast<routine_type>(lpStartAddress),
+        lpParameter,
+        dwCreationFlags,
+        reinterpret_cast<unsigned*>(lpThreadId)));
+
+    return M2GetLastHResultError(*lpThreadHandle != nullptr);
+}
+
+DWORD M2GetNumberOfHardwareThreads()
+{
+    SYSTEM_INFO SystemInfo = { 0 };
+    GetNativeSystemInfo(&SystemInfo);
+    return SystemInfo.dwNumberOfProcessors;
+}
+
+ULONGLONG M2GetTickCount()
+{
+    LARGE_INTEGER Frequency = { 0 }, PerformanceCount = { 0 };
+
+    if (QueryPerformanceFrequency(&Frequency))
+    {
+        if (QueryPerformanceCounter(&PerformanceCount))
+        {
+            return (PerformanceCount.QuadPart * 1000 / Frequency.QuadPart);
+        }
+    }
+
+    return GetTickCount64();
+}
+
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP | WINAPI_PARTITION_SYSTEM)
+
+HRESULT M2CreateFile(
+    _Out_ PHANDLE lpFileHandle,
+    _In_ LPCWSTR lpFileName,
+    _In_ DWORD dwDesiredAccess,
+    _In_ DWORD dwShareMode,
+    _In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    _In_ DWORD dwCreationDisposition,
+    _In_ DWORD dwFlagsAndAttributes,
+    _In_opt_ HANDLE hTemplateFile)
+{
+    *lpFileHandle = CreateFileW(
+        lpFileName,
+        dwDesiredAccess,
+        dwShareMode,
+        lpSecurityAttributes,
+        dwCreationDisposition,
+        dwFlagsAndAttributes,
+        hTemplateFile);
+
+    return M2GetLastHResultError(INVALID_HANDLE_VALUE != *lpFileHandle);
+}
+
+#endif
+
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP | WINAPI_PARTITION_SYSTEM)
+
+HRESULT M2RegCloseKey(
+    _In_ HKEY hKey)
+{
+    return HRESULT_FROM_WIN32(RegCloseKey(hKey));
+}
+
+HRESULT M2RegCreateKey(
+    _In_ HKEY hKey,
+    _In_ LPCWSTR lpSubKey,
+    _Reserved_ DWORD Reserved,
+    _In_opt_ LPWSTR lpClass,
+    _In_ DWORD dwOptions,
+    _In_ REGSAM samDesired,
+    _In_opt_ CONST LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    _Out_ PHKEY phkResult,
+    _Out_opt_ LPDWORD lpdwDisposition)
+{
+    return HRESULT_FROM_WIN32(RegCreateKeyExW(
+        hKey,
+        lpSubKey,
+        Reserved,
+        lpClass,
+        dwOptions,
+        samDesired,
+        lpSecurityAttributes,
+        phkResult,
+        lpdwDisposition));
+}
+
+HRESULT M2RegQueryValue(
+    _In_ HKEY hKey,
+    _In_opt_ LPCWSTR lpValueName,
+    _Reserved_ LPDWORD lpReserved,
+    _Out_opt_ LPDWORD lpType,
+    _Out_opt_ LPBYTE lpData,
+    _Inout_opt_ LPDWORD lpcbData)
+{
+    return HRESULT_FROM_WIN32(RegQueryValueExW(
+        hKey,
+        lpValueName,
+        lpReserved,
+        lpType,
+        lpData,
+        lpcbData));
+}
+
+HRESULT M2RegSetValue(
+    _In_ HKEY hKey,
+    _In_opt_ LPCWSTR lpValueName,
+    _Reserved_ DWORD Reserved,
+    _In_ DWORD dwType,
+    _In_opt_ CONST BYTE* lpData,
+    _In_ DWORD cbData)
+{
+    return HRESULT_FROM_WIN32(RegSetValueExW(
+        hKey,
+        lpValueName,
+        Reserved,
+        dwType,
+        lpData,
+        cbData));
+}
+
+#endif
+
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP | WINAPI_PARTITION_SYSTEM)
+
+HRESULT M2LoadLibrary(
+    _Out_ HMODULE* phLibModule,
+    _In_ LPCWSTR lpLibFileName,
+    _Reserved_ HANDLE hFile,
+    _In_ DWORD dwFlags)
+{
+    *phLibModule = LoadLibraryExW(lpLibFileName, hFile, dwFlags);
+    return M2GetLastHResultError(*phLibModule != nullptr);
+}
+
+#endif
+
+HRESULT M2FreeLibrary(
+    _In_ HMODULE hLibModule)
+{
+    return M2GetLastHResultError(FreeLibrary(hLibModule));
+}
+
+HRESULT M2GetProcAddress(
+    _Out_ FARPROC* lpProcAddress,
+    _In_ HMODULE hModule,
+    _In_ LPCSTR lpProcName)
+{
+    *lpProcAddress = GetProcAddress(hModule, lpProcName);
+    return M2GetLastHResultError(*lpProcAddress != nullptr);
+}
+
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP | WINAPI_PARTITION_SYSTEM)
+
+HRESULT M2DeviceIoControl(
+    _In_ HANDLE hDevice,
+    _In_ DWORD dwIoControlCode,
+    _In_opt_ LPVOID lpInBuffer,
+    _In_ DWORD nInBufferSize,
+    _Out_opt_ LPVOID lpOutBuffer,
+    _In_ DWORD nOutBufferSize,
+    _Out_opt_ LPDWORD lpBytesReturned,
+    _Inout_opt_ LPOVERLAPPED lpOverlapped)
+{
+    return M2GetLastHResultError(DeviceIoControl(
+        hDevice,
+        dwIoControlCode,
+        lpInBuffer,
+        nInBufferSize,
+        lpOutBuffer,
+        nOutBufferSize,
+        lpBytesReturned,
+        lpOverlapped));
+}
+
+#endif
+
+HRESULT M2GetFileInformation(
+    _In_  HANDLE hFile,
+    _In_  FILE_INFO_BY_HANDLE_CLASS FileInformationClass,
+    _Out_ LPVOID lpFileInformation,
+    _In_  DWORD dwBufferSize)
+{
+    return M2GetLastHResultError(GetFileInformationByHandleEx(
+        hFile,
+        FileInformationClass,
+        lpFileInformation,
+        dwBufferSize));
+}
+
+HRESULT M2SetFileInformation(
+    _In_ HANDLE hFile,
+    _In_ FILE_INFO_BY_HANDLE_CLASS FileInformationClass,
+    _In_ LPVOID lpFileInformation,
+    _In_ DWORD dwBufferSize)
+{
+    return M2GetLastHResultError(SetFileInformationByHandle(
+        hFile,
+        FileInformationClass,
+        lpFileInformation,
+        dwBufferSize));
+}
+
+HRESULT M2CoCreateInstance(
+    _In_ LPCWSTR lpszCLSID,
+    _In_opt_ LPUNKNOWN pUnkOuter,
+    _In_ DWORD dwClsContext,
+    _In_ LPCWSTR lpszIID,
+    _Out_ LPVOID* ppv)
+{
+    CLSID clsid;
+    IID iid;
+
+    HRESULT hr = CLSIDFromString(lpszCLSID, &clsid);
+    if (SUCCEEDED(hr))
+    {
+        hr = IIDFromString(lpszIID, &iid);
+        if (SUCCEEDED(hr))
+        {
+            hr = CoCreateInstance(clsid, pUnkOuter, dwClsContext, iid, ppv);
+        }
+    }
+
+    return hr;
+}
+
+HRESULT M2AdjustTokenPrivileges(
+    _In_ HANDLE TokenHandle,
+    _In_ BOOL DisableAllPrivileges,
+    _In_opt_ PTOKEN_PRIVILEGES NewState,
+    _In_ DWORD BufferLength,
+    _Out_opt_ PTOKEN_PRIVILEGES PreviousState,
+    _Out_opt_ PDWORD ReturnLength)
+{
+    return M2GetLastHResultError(
+        AdjustTokenPrivileges(
+            TokenHandle,
+            DisableAllPrivileges,
+            NewState,
+            BufferLength,
+            PreviousState,
+            ReturnLength),
+        TRUE);
+}
+
+HRESULT M2GetTokenInformation(
+    _In_ HANDLE TokenHandle,
+    _In_ TOKEN_INFORMATION_CLASS TokenInformationClass,
+    _Out_opt_ LPVOID TokenInformation,
+    _In_ DWORD TokenInformationLength,
+    _Out_ PDWORD ReturnLength)
+{
+    return M2GetLastHResultError(GetTokenInformation(
+        TokenHandle,
+        TokenInformationClass,
+        TokenInformation,
+        TokenInformationLength,
+        ReturnLength));
+}
 
 #endif // _M2_WINDOWS_DOWNLEVEL_HELPERS_
