b3d5f359daf7d7ffa4aaa0c9a1ea5b6b7e844fe2
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-01-04 15:43:53 +0800

    Add Detours.

--- NSudoDevilMode/NSudoDevilMode.cpp
+++ NSudoDevilMode/NSudoDevilMode.cpp
@@ -1,7 +1,7 @@
 ﻿/*
  * PROJECT:   NSudo Devil Mode
- * FILE:      DllMain.cpp
- * PURPOSE:   DllMain Implementation for NSudo Devil Mode
+ * FILE:      NSudoDevilMode.cpp
+ * PURPOSE:   Implementation for NSudo Devil Mode
  *
  * LICENSE:   The MIT License
  *
@@ -10,24 +10,54 @@
 
 #include <Windows.h>
 
-BOOL APIENTRY DllMain(
-    HMODULE hModule,
-    DWORD  ul_reason_for_call,
-    LPVOID lpReserved)
-{
-    UNREFERENCED_PARAMETER(hModule);
-    UNREFERENCED_PARAMETER(lpReserved);
+#include "Detours/detours.h"
 
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        break;
-    case DLL_THREAD_ATTACH:
-        break;
-    case DLL_THREAD_DETACH:
-        break;
-    case DLL_PROCESS_DETACH:
-        break;
+static LONG dwSlept = 0;
+
+// Target pointer for the uninstrumented Sleep API.
+//
+static decltype(Sleep)* TrueSleep = Sleep;
+
+// Detour function that replaces the Sleep API.
+//
+VOID WINAPI TimedSleep(DWORD dwMilliseconds)
+{
+    // Save the before and after times around calling the Sleep API.
+    DWORD dwBeg = GetTickCount();
+    TrueSleep(dwMilliseconds);
+    DWORD dwEnd = GetTickCount();
+
+    InterlockedExchangeAdd(&dwSlept, dwEnd - dwBeg);
+}
+
+BOOL APIENTRY DllMain(
+    HMODULE Module,
+    DWORD  Reason,
+    LPVOID Reserved)
+{
+    UNREFERENCED_PARAMETER(Module);
+    UNREFERENCED_PARAMETER(Reserved);
+
+    if (DetourIsHelperProcess()) {
+        return TRUE;
     }
+
+    if (DLL_PROCESS_ATTACH == Reason)
+    {
+        DetourRestoreAfterWith();
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&reinterpret_cast<PVOID&>(TrueSleep), TimedSleep);
+        DetourTransactionCommit();
+    }
+    else if (DLL_PROCESS_DETACH == Reason)
+    {
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourDetach(&reinterpret_cast<PVOID&>(TrueSleep), TimedSleep);
+        DetourTransactionCommit();
+    }
+
     return TRUE;
 }
