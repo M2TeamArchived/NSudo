3c483cadefa591cf00572027a85d34fe2db7e5fd
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-01-04 15:19:23 +0800

    Add NSudo Devil Mode.

---
+++ NSudoDevilMode/NSudoDevilMode.cpp
@@ -0,0 +1,33 @@
+﻿/*
+ * PROJECT:   NSudo Devil Mode
+ * FILE:      DllMain.cpp
+ * PURPOSE:   DllMain Implementation for NSudo Devil Mode
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <Windows.h>
+
+BOOL APIENTRY DllMain(
+    HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved)
+{
+    UNREFERENCED_PARAMETER(hModule);
+    UNREFERENCED_PARAMETER(lpReserved);
+
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
