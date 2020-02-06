d0fc22b873b2596974d8439c1d62943c22a3c0c5
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-02-06 21:47:12 +0800

    Add MileAllocMemory, MileReAllocMemory and MileFreeMemory.

--- NSudoLib/Mile.Windows.cpp
+++ NSudoLib/Mile.Windows.cpp
@@ -9,3 +9,41 @@
  */
 
 #include "Mile.Windows.h"
+
+/**
+ * @remark You can read the definition for this function in "Mile.Windows.h".
+ */
+EXTERN_C HRESULT WINAPI MileAllocMemory(
+    _In_ SIZE_T Size,
+    _Out_ LPVOID* Block)
+{
+    *Block = ::HeapAlloc(::GetProcessHeap(), HEAP_ZERO_MEMORY, Size);
+    return *Block ? S_OK : ::HRESULT_FROM_WIN32(ERROR_NOT_ENOUGH_MEMORY);
+}
+
+/**
+ * @remark You can read the definition for this function in "Mile.Windows.h".
+ */
+EXTERN_C HRESULT WINAPI MileReAllocMemory(
+    _In_ PVOID OldBlock,
+    _In_ SIZE_T NewSize,
+    _Out_ PVOID* NewBlock)
+{
+    *NewBlock = ::HeapReAlloc(
+        ::GetProcessHeap(), HEAP_ZERO_MEMORY, OldBlock, NewSize);
+    return *NewBlock ? S_OK : ::HRESULT_FROM_WIN32(ERROR_NOT_ENOUGH_MEMORY);
+}
+
+/**
+ * @remark You can read the definition for this function in "Mile.Windows.h".
+ */
+EXTERN_C HRESULT WINAPI MileFreeMemory(
+    _In_ LPVOID Block)
+{
+    if (!::HeapFree(::GetProcessHeap(), 0, Block))
+    {
+        return ::HRESULT_FROM_WIN32(::GetLastError());
+    }
+
+    return S_OK;
+}
