dd5350b41fe955b7900e0dc742822dc3a6af6335
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-02-06 21:25:43 +0800

    Create Mile (Mouri Internal Library Essentials).

---
+++ NSudoLib/Mile.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.cpp
+ * PURPOSE:   Implementation for Mouri Internal Library Essentials
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.h"
---
+++ NSudoLib/Mile.h
@@ -0,0 +1,23 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.h
+ * PURPOSE:   Definition for Mouri Internal Library Essentials
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#ifndef MILE
+#define MILE
+
+#ifndef __cplusplus
+#error "[Mile] You should use a C++ compiler."
+#endif // !__cplusplus
+
+#include <Windows.h>
+
+
+
+#endif // !MILE
+
