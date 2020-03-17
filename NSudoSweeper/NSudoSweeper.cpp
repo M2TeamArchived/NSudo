a2c4638c0b5b15d3af43a894ce284605ac944666
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-03-18 04:56:49 +0800

    Add some control in NSudo Sweeper for learning the WTL.

--- NSudoSweeper/NSudoSweeper.cpp
+++ NSudoSweeper/NSudoSweeper.cpp
@@ -28,6 +28,10 @@ private:
 
     CStatic ContentControl;
 
+    CListViewCtrl ItemList;
+
+    CBrush MenuBackgroundBrush;
+
 public:
 
     BEGIN_MSG_MAP(CMainWindow)
@@ -70,6 +74,40 @@ public:
 
         this->ContentControl.SetFont(UIFont);
 
+        auto y = CRect(50, 200, 400, 300);
+
+        this->ItemList.Create(
+            this->m_hWnd,
+            y,
+            L"Test it",
+            WS_CHILD | WS_VISIBLE | LVS_REPORT);
+
+        this->ItemList.AddColumn(L"Name", 0);
+        this->ItemList.AddColumn(L"Description", 1);
+
+        this->ItemList.AddItem(0, 0, L"God");
+
+        this->ItemList.AddItem(0, 1, L"Sake");
+
+        CMenu HelpMenu;
+        HelpMenu.CreatePopupMenu();
+        HelpMenu.AppendMenuW(MF_STRING | MF_ENABLED, 2000, L"&Content");
+        HelpMenu.AppendMenuW(MF_STRING | MF_ENABLED, 2001, L"&About");
+
+        CMenu WindowMenu;
+        WindowMenu.CreateMenu();
+        WindowMenu.AppendMenuW(MF_STRING | MF_ENABLED, HelpMenu, L"&Help");
+        
+        MenuBackgroundBrush.CreateSolidBrush(RGB(255, 255, 255));
+        MENUINFO WindowMenuInfo = { 0 };
+        WindowMenuInfo.cbSize = sizeof(MENUINFO);
+        WindowMenuInfo.fMask = MIM_APPLYTOSUBMENUS | MIM_BACKGROUND;
+        WindowMenuInfo.hbrBack = MenuBackgroundBrush;
+
+        WindowMenu.SetMenuInfo(&WindowMenuInfo);
+
+        this->SetMenu(WindowMenu);
+        
         return 0;
     }
 
@@ -91,6 +129,7 @@ int WINAPI wWinMain(
     _In_ LPWSTR lpCmdLine,
     _In_ int nShowCmd)
 {
+    UNREFERENCED_PARAMETER(hInstance);
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
