1df6791dbdb30f6c92e32a1d882b1dc2520ef6f9
MouriNaruto <Mouri_Naruto@outlook.com>
2020-05-19 21:21:32 +0800

    Implement a basic layout viewer.

---
+++ src/M2.NSudo.Prototype.IntegratedUserExperience/LayoutPreviewControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M2.NSudo.Prototype.IntegratedUserExperience
+{
+    /// <summary>
+    /// LayoutPreviewControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class LayoutPreviewControl : UserControl
+    {
+        public LayoutPreviewControl()
+        {
+            InitializeComponent();
+        }
+
+        private void LayoutPreviewControlInternal_LayoutUpdated(object sender, EventArgs e)
+        {
+            try
+            {
+                FrameworkElement ParentWindow = (FrameworkElement)Window.GetWindow(this).Content;
+
+                Point CurrentPoint = TransformToAncestor(ParentWindow).Transform(new Point(0, 0));
+
+                Thickness LayoutValue = new Thickness(
+                    CurrentPoint.X,
+                    CurrentPoint.Y,
+                    ParentWindow.ActualWidth - CurrentPoint.X - ActualWidth,
+                    ParentWindow.ActualHeight - CurrentPoint.Y - ActualHeight);
+
+                MarginText.Text = LayoutValue.ToString();
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+    }
+}
