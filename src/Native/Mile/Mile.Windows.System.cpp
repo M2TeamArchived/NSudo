8e464aa5059368413d2f189594acb32539b93618
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-09-17 01:42:18 +0800

    Add Mile::Windows::System infrastructures.

---
+++ src/Native/Mile/Mile.Windows.System.cpp
@@ -0,0 +1,30 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.System.cpp
+ * PURPOSE:   System Implementation for Windows
+ *
+ * LICENSE:   Apache-2.0 License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.Windows.System.h"
+
+LPVOID Mile::Windows::System::AllocateMemory(
+    _In_ SIZE_T Size)
+{
+    return ::HeapAlloc(::GetProcessHeap(), HEAP_ZERO_MEMORY, Size);
+}
+
+LPVOID Mile::Windows::System::ReallocateMemory(
+    _In_ PVOID Block,
+    _In_ SIZE_T Size)
+{
+    return ::HeapReAlloc(::GetProcessHeap(), HEAP_ZERO_MEMORY, Block, Size);
+}
+
+BOOL Mile::Windows::System::FreeMemory(
+    _In_ LPVOID Block)
+{
+    ::HeapFree(::GetProcessHeap(), 0, Block);
+}
