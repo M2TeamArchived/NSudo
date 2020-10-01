7b75fb301cb8c3ef0ef0a9a3d2c1dbeab89fd35f
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-10-02 01:20:30 +0800

    Add Mile.Portable.

---
+++ src/Native/Mile/Mile.Portable.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Portable.cpp
+ * PURPOSE:   Portable Implementation
+ *
+ * LICENSE:   Apache-2.0 License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.Portable.h"
---
+++ src/Native/Mile/Mile.Portable.h
@@ -0,0 +1,55 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Portable.h
+ * PURPOSE:   Portable Definition
+ *
+ * LICENSE:   Apache-2.0 License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#ifndef MILE_PORTABLE
+#define MILE_PORTABLE
+
+#if (defined(__cplusplus) && __cplusplus >= 201703L)
+#elif (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
+#else
+#error "[Mile] You should use a C++ compiler with the C++17 standard."
+#endif
+
+namespace Mile
+{
+    /**
+     * @brief Disables C++ class copy construction.
+    */
+    class DisableCopyConstruction
+    {
+    protected:
+        DisableCopyConstruction() = default;
+        ~DisableCopyConstruction() = default;
+
+    private:
+        DisableCopyConstruction(
+            const DisableCopyConstruction&) = delete;
+        DisableCopyConstruction& operator=(
+            const DisableCopyConstruction&) = delete;
+    };
+
+    /**
+     * @brief Disables C++ class move construction.
+    */
+    class DisableMoveConstruction
+    {
+    protected:
+        DisableMoveConstruction() = default;
+        ~DisableMoveConstruction() = default;
+
+    private:
+        DisableMoveConstruction(
+            const DisableMoveConstruction&&) = delete;
+        DisableMoveConstruction& operator=(
+            const DisableMoveConstruction&&) = delete;
+    };
+}
+
+#endif // !MILE_PORTABLE
