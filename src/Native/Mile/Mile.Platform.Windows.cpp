e8a7c40e95209c3b3f737886d2bc2c4178e1a413
Naruto Mouri <Mouri_Naruto@outlook.com>
2020-10-02 01:56:18 +0800

    Add Mile.Platform.Windows.

---
+++ src/Native/Mile/Mile.Platform.Windows.cpp
@@ -0,0 +1,30 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Platform.Windows.cpp
+ * PURPOSE:   Windows Platform Implementation
+ *
+ * LICENSE:   Apache-2.0 License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.Platform.Windows.h"
+
+LPVOID Mile::HeapMemory::Allocate(
+    _In_ SIZE_T Size)
+{
+    return ::HeapAlloc(::GetProcessHeap(), HEAP_ZERO_MEMORY, Size);
+}
+
+LPVOID Mile::HeapMemory::Reallocate(
+    _In_ PVOID Block,
+    _In_ SIZE_T Size)
+{
+    return ::HeapReAlloc(::GetProcessHeap(), HEAP_ZERO_MEMORY, Block, Size);
+}
+
+BOOL Mile::HeapMemory::Free(
+    _In_ LPVOID Block)
+{
+    return ::HeapFree(::GetProcessHeap(), 0, Block);
+}
---
+++ src/Native/Mile/Mile.Platform.Windows.h
@@ -0,0 +1,81 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Platform.Windows.h
+ * PURPOSE:   Windows Platform Definition
+ *
+ * LICENSE:   Apache-2.0 License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#ifndef MILE_PLATFORM_WINDOWS
+#define MILE_PLATFORM_WINDOWS
+
+#if (defined(__cplusplus) && __cplusplus >= 201703L)
+#elif (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
+#else
+#error "[Mile] You should use a C++ compiler with the C++17 standard."
+#endif
+
+#include "Mile.Platform.h"
+
+#include <Windows.h>
+
+namespace Mile
+{
+    /**
+     * @brief Wraps the Windows heap memory manager.
+    */
+    class HeapMemory
+    {
+    public:
+
+        /**
+         * @brief Allocates a block of memory from the default heap of the
+         *        calling process. The allocated memory will be initialized to
+         *        zero. The allocated memory is not movable.
+         * @param Size The number of bytes to be allocated.
+         * @return If the function succeeds, the return value is a pointer to
+         *         the allocated memory block. If the function fails, the
+         *         return value is nullptr.
+        */
+        static LPVOID Allocate(
+            _In_ SIZE_T Size);
+
+        /**
+         * @brief Reallocates a block of memory from the default heap of the
+         *        calling process. If the reallocation request is for a larger
+         *        size, the additional region of memory beyond the original
+         *        size be initialized to zero. This function enables you to
+         *        resize a memory block and change other memory block
+         *        properties. The allocated memory is not movable.
+         * @param Block A pointer to the block of memory that the function
+         *              reallocates. This pointer is returned by an earlier
+         *              call to Allocate and Reallocate method.
+         * @param Size The new size of the memory block, in bytes. A memory
+         *             block's size can be increased or decreased by using this
+         *             function.
+         * @return If the function succeeds, the return value is a pointer to
+         *         the reallocated memory block. If the function fails, the
+         *         return value is nullptr.
+        */
+        static LPVOID Reallocate(
+            _In_ PVOID Block,
+            _In_ SIZE_T Size);
+
+        /**
+         * @brief Frees a memory block allocated from a heap by the Allocate or
+         *        Reallocate method.
+         * @param Block A pointer to the memory block to be freed. This pointer
+         *              is returned by the Allocate or Reallocate method. If
+         *              this pointer is nullptr, the behavior is undefined.
+         * @return If the function succeeds, the return value is nonzero. If
+         *         the function fails, the return value is zero. An application
+         *         can call GetLastError for extended error information.
+        */
+        static BOOL Free(
+            _In_ LPVOID Block);
+    };
+}
+
+#endif // !MILE_PLATFORM_WINDOWS
