0e7603d6010497fee668eff5f39564903c465e46
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-01-23 18:39:50 +0800

    Add Mile::HResult.

--- src/Native/Mile/Mile.Windows.Core.h
+++ src/Native/Mile/Mile.Windows.Core.h
@@ -19,6 +19,106 @@
 
 #include <Windows.h>
 
+#include <string>
+
+namespace Mile
+{
+    /**
+     * @brief A type representing an HRESULT error code.
+    */
+    class HResult
+    {
+    public:
+
+        /**
+         * @brief The HRESULT error code represented by the HResult object.
+        */
+        HRESULT Value{ S_OK };
+
+    public:
+
+        /**
+         * @brief Initializes a new instance of the HResult object.
+         * @return A new instance of the HResult object.
+        */
+        constexpr HResult() noexcept = default;
+
+        /**
+         * @brief Initializes a new instance of the HResult object by an
+         *        HRESULT code.
+         * @param Value An HRESULT code that initializes the HResult object.
+         * @return A new instance of the HResult object.
+        */
+        constexpr HResult(
+            _In_ HRESULT const Value) noexcept : Value(Value)
+        {
+        }
+
+        /**
+         * @brief Initializes a new instance of the HResult object by a system
+         *        error code.
+         * @param Code The system error code.
+         * @return A new instance of the HResult object.
+        */
+        static HResult FromWin32(
+            _In_ DWORD Code) noexcept
+        {
+            return HResult(::HRESULT_FROM_WIN32(Code));
+        }
+
+        /**
+         * @brief Initializes a new instance of the HResult object by the
+         *        calling thread's last-error code.
+         * @return A new instance of the HResult object.
+        */
+        static HResult FromLastError() noexcept
+        {
+            return FromWin32(::GetLastError());
+        }
+
+    public:
+
+        /**
+         * @brief Retrieves the HRESULT error code for the error represented by
+         *        the HResult object.
+         * @return An HRESULT error code.
+        */
+        constexpr operator HRESULT() const noexcept
+        {
+            return this->Value;
+        }
+
+        /**
+         * @brief Retrieves the message for the error represented by the
+         *        HResult object.
+         * @return A std::wstring containing the error messsage.
+        */
+        std::wstring ToMessage() const noexcept
+        {
+            std::wstring Message{ L"Failed to get formatted message." };
+
+            LPWSTR RawMessage = nullptr;
+            DWORD RawMessageSize = ::FormatMessageW(
+                FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                FORMAT_MESSAGE_FROM_SYSTEM |
+                FORMAT_MESSAGE_IGNORE_INSERTS,
+                nullptr,
+                this->Value,
+                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                reinterpret_cast<LPTSTR>(&RawMessage),
+                0,
+                nullptr);
+            if (RawMessageSize)
+            {
+                Message = std::wstring(RawMessage, RawMessageSize);
+
+                ::LocalFree(RawMessage);
+            }
+
+            return Message;
+        }
+    };
+}
 
 
 #endif // !MILE_WINDOWS_CORE
