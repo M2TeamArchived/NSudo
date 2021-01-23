c864ec34a9114a26344b761f1bdf6d9a260a3d63
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-01-23 17:50:08 +0800

    Add Mile.Windows.Core.

---
+++ src/Native/Mile/Mile.Windows.Core.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.Core.cpp
+ * PURPOSE:   Core Implementation for Windows
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.Windows.Core.h"
---
+++ src/Native/Mile/Mile.Windows.Core.h
@@ -0,0 +1,24 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.Core.h
+ * PURPOSE:   Core Definition for Windows
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#ifndef MILE_WINDOWS_CORE
+#define MILE_WINDOWS_CORE
+
+#if (defined(__cplusplus) && __cplusplus >= 201703L)
+#elif (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
+#else
+#error "[Mile] You should use a C++ compiler with the C++17 standard."
+#endif
+
+#include <Windows.h>
+
+
+
+#endif // !MILE_WINDOWS_CORE
