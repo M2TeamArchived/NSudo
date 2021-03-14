cadf2287db34a41effbdb92735eb606bca9e0494
MouriNaruto <Mouri_Naruto@outlook.com>
2021-03-14 17:37:40 +0800

    Add Mile::DeviceIoControl, Mile::GetNtfsCompressionAttribute, Mile::SetNtfsCompressionAttribute, Mile::GetWofCompressionAttribute, Mile::SetWofCompressionAttribute and Mile::RemoveWofCompressionAttribute. Remove Mile::Windows::Storage::DeviceIoControl, Mile::Windows::Storage::GetNtfsCompressionAttribute, Mile::Windows::Storage::SetNtfsCompressionAttribute, Mile::Windows::Storage::GetWofCompressionAttribute, Mile::Windows::Storage::SetWofCompressionAttribute and Mile::Windows::Storage::RemoveWofCompressionAttribute.

--- src/Native/Mile/Mile.Windows.Core.cpp
+++ src/Native/Mile/Mile.Windows.Core.cpp
@@ -12,10 +12,188 @@
 
 #pragma region Implementations for Windows (Win32 Style)
 
+namespace
+{
+    /**
+     * @brief The information about the Windows Overlay Filter file provider.
+    */
+    typedef struct _WOF_FILE_PROVIDER_EXTERNAL_INFO
+    {
+        WOF_EXTERNAL_INFO Wof;
+        FILE_PROVIDER_EXTERNAL_INFO FileProvider;
+    } WOF_FILE_PROVIDER_EXTERNAL_INFO, * PWOF_FILE_PROVIDER_EXTERNAL_INFO;
+}
+
 Mile::HResultFromLastError Mile::CloseHandle(
     _In_ HANDLE hObject)
 {
     return ::CloseHandle(hObject);
+}
+
+Mile::HResultFromLastError Mile::DeviceIoControl(
+    _In_ HANDLE hDevice,
+    _In_ DWORD dwIoControlCode,
+    _In_opt_ LPVOID lpInBuffer,
+    _In_ DWORD nInBufferSize,
+    _Out_opt_ LPVOID lpOutBuffer,
+    _In_ DWORD nOutBufferSize,
+    _Out_opt_ LPDWORD lpBytesReturned)
+{
+    BOOL Result = FALSE;
+    OVERLAPPED Overlapped = { 0 };
+    Overlapped.hEvent = ::CreateEventW(
+        nullptr,
+        TRUE,
+        FALSE,
+        nullptr);
+    if (Overlapped.hEvent)
+    {
+        Result = ::DeviceIoControl(
+            hDevice,
+            dwIoControlCode,
+            lpInBuffer,
+            nInBufferSize,
+            lpOutBuffer,
+            nOutBufferSize,
+            lpBytesReturned,
+            &Overlapped);
+        if (!Result)
+        {
+            if (::GetLastError() == ERROR_IO_PENDING)
+            {
+                Result = ::GetOverlappedResult(
+                    hDevice,
+                    &Overlapped,
+                    lpBytesReturned,
+                    TRUE);
+            }
+        }
+
+        ::CloseHandle(Overlapped.hEvent);
+    }
+    else
+    {
+        ::SetLastError(ERROR_NO_SYSTEM_RESOURCES);
+    }
+
+    return Result;
+}
+
+Mile::HResultFromLastError Mile::GetNtfsCompressionAttribute(
+    _In_ HANDLE FileHandle,
+    _Out_ PUSHORT CompressionAlgorithm)
+{
+    if (!CompressionAlgorithm)
+        return E_INVALIDARG;
+
+    DWORD BytesReturned;
+    return Mile::DeviceIoControl(
+        FileHandle,
+        FSCTL_GET_COMPRESSION,
+        nullptr,
+        0,
+        CompressionAlgorithm,
+        sizeof(*CompressionAlgorithm),
+        &BytesReturned);
+}
+
+Mile::HResultFromLastError Mile::SetNtfsCompressionAttribute(
+    _In_ HANDLE FileHandle,
+    _In_ USHORT CompressionAlgorithm)
+{
+    switch (CompressionAlgorithm)
+    {
+    case COMPRESSION_FORMAT_NONE:
+    case COMPRESSION_FORMAT_DEFAULT:
+    case COMPRESSION_FORMAT_LZNT1:
+        break;
+    default:
+        return E_INVALIDARG;
+    }
+
+    DWORD BytesReturned;
+    return Mile::DeviceIoControl(
+        FileHandle,
+        FSCTL_SET_COMPRESSION,
+        &CompressionAlgorithm,
+        sizeof(CompressionAlgorithm),
+        nullptr,
+        0,
+        &BytesReturned);
+}
+
+Mile::HResultFromLastError Mile::GetWofCompressionAttribute(
+    _In_ HANDLE FileHandle,
+    _Out_ PDWORD CompressionAlgorithm)
+{
+    if (!CompressionAlgorithm)
+        return E_INVALIDARG;
+
+    WOF_FILE_PROVIDER_EXTERNAL_INFO WofInfo = { 0 };
+    DWORD BytesReturned;
+    if (!Mile::DeviceIoControl(
+        FileHandle,
+        FSCTL_GET_EXTERNAL_BACKING,
+        nullptr,
+        0,
+        &WofInfo,
+        sizeof(WofInfo),
+        &BytesReturned))
+    {
+        return FALSE;
+    }
+
+    *CompressionAlgorithm = WofInfo.FileProvider.Algorithm;
+    return TRUE;
+}
+
+Mile::HResultFromLastError Mile::SetWofCompressionAttribute(
+    _In_ HANDLE FileHandle,
+    _In_ DWORD CompressionAlgorithm)
+{
+    switch (CompressionAlgorithm)
+    {
+    case FILE_PROVIDER_COMPRESSION_XPRESS4K:
+    case FILE_PROVIDER_COMPRESSION_LZX:
+    case FILE_PROVIDER_COMPRESSION_XPRESS8K:
+    case FILE_PROVIDER_COMPRESSION_XPRESS16K:
+        break;
+    default:
+        return E_INVALIDARG;
+    }
+
+    WOF_FILE_PROVIDER_EXTERNAL_INFO WofInfo = { 0 };
+
+    WofInfo.Wof.Version = WOF_CURRENT_VERSION;
+    WofInfo.Wof.Provider = WOF_PROVIDER_FILE;
+
+    WofInfo.FileProvider.Version = FILE_PROVIDER_CURRENT_VERSION;
+    WofInfo.FileProvider.Flags = 0;
+    WofInfo.FileProvider.Algorithm = CompressionAlgorithm;
+
+    DWORD BytesReturned;
+    return Mile::DeviceIoControl(
+        FileHandle,
+        FSCTL_SET_EXTERNAL_BACKING,
+        &WofInfo,
+        sizeof(WofInfo),
+        nullptr,
+        0,
+        &BytesReturned);
+}
+
+Mile::HResultFromLastError Mile::RemoveWofCompressionAttribute(
+    _In_ HANDLE FileHandle)
+{
+    DWORD BytesReturned;
+    return Mile::DeviceIoControl(
+        FileHandle,
+        FSCTL_DELETE_EXTERNAL_BACKING,
+        nullptr,
+        0,
+        nullptr,
+        0,
+        &BytesReturned);
 }
 
 #pragma endregion
