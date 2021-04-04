702e863f0e5dd5a90dd1bedb7e445707fb39b6b5
MouriNaruto <Mouri_Naruto@outlook.com>
2021-04-04 14:46:51 +0800

    Add Mile.Portable.

---
+++ src/Native/Mile/Mile.Portable.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Portable.cpp
+ * PURPOSE:   Implementation for all platforms
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.Portable.h"
---
+++ src/Native/Mile/Mile.Portable.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Portable.h
+ * PURPOSE:   Definition for all platforms
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#ifndef MILE_PORTABLE
+#define MILE_PORTABLE
+
+
+
+#endif // !MILE_PORTABLE
