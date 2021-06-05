aca327d389ebef56f8df6399e2894deaa60651cd
MouriNaruto <Mouri_Naruto@outlook.com>
2021-06-05 21:19:18 +0800

    Try to continue to fix CI.

--- src/Native/BuildAllTargets.cmd
+++ src/Native/BuildAllTargets.cmd
@@ -28,9 +28,6 @@ popd
 
 call "%VisualStudioInstallDir%\VC\Auxiliary\Build\vcvarsall.bat" x86
 
-rem Restore all targets
-MSBuild -r -t:Restore -p:Configuration=Release;Platform=Win32 BuildAllTargets.proj
-
 rem Build all targets
 MSBuild -m BuildAllTargets.proj
 
