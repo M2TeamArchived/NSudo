2d2f9c021242202815f4e5f330e7039cc677c902
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-07-24 23:03:56 +0800

    Improve the usability of MoPurgeNuGetCache.

--- Source/Native/MouriOptimizationPlugin/MoPurgeNuGetCache.cpp
+++ Source/Native/MouriOptimizationPlugin/MoPurgeNuGetCache.cpp
@@ -10,8 +10,175 @@
 
 #include "MouriOptimizationPlugin.h"
 
+#include <regex>
 #include <vector>
 #include <string>
+
+namespace
+{
+    static const std::wregex g_NuGetPackageCacheFileInclusion =
+        std::wregex(
+            L"(.*)\\\\.nuget\\\\packages\\\\(.*)\\\\(.*)\\\\(.*).nupkg",
+            std::regex_constants::syntax_option_type::icase);
+
+    static void PurgeNuGetPackageCacheWorker(
+        _In_ PNSUDO_CONTEXT Context,
+        _In_ LPCWSTR RootPath,
+        _Inout_opt_ PUINT64 UsedSpace)
+    {
+        HANDLE RootHandle = ::MoPrivateCreateFile(
+            RootPath,
+            SYNCHRONIZE |
+            FILE_LIST_DIRECTORY |
+            DELETE |
+            FILE_READ_ATTRIBUTES |
+            FILE_WRITE_ATTRIBUTES,
+            FILE_SHARE_READ | FILE_SHARE_WRITE,
+            nullptr,
+            OPEN_EXISTING,
+            FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+            nullptr);
+        if (RootHandle != INVALID_HANDLE_VALUE)
+        {
+            Mile::HResult hr = S_OK;
+            hr = Mile::EnumerateFile(
+                RootHandle,
+                [&](
+                    _In_ Mile::PFILE_ENUMERATE_INFORMATION Information) -> BOOL
+            {
+                if (Mile::IsDotsName(Information->FileName))
+                {
+                    return TRUE;
+                }
+
+                std::wstring CurrentPath = Mile::FormatUtf16String(
+                    L"%s\\%s",
+                    RootPath,
+                    Information->FileName);
+
+                if (Information->FileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+                {
+                    ::PurgeNuGetPackageCacheWorker(
+                        Context,
+                        CurrentPath.c_str(),
+                        UsedSpace);
+                    return TRUE;
+                }
+
+                std::wsmatch Matches;
+                if (!std::regex_match(
+                    CurrentPath,
+                    Matches,
+                    g_NuGetPackageCacheFileInclusion))
+                {
+                    return TRUE;
+                }
+
+                // The cleanable file path in NuGet package folder should be
+                // the form of .nuget\packages\[C]\[V]\[C].[V].nupkg.
+                if (::_wcsicmp(
+                    (Matches[2].str() + L"." + Matches[3].str()).c_str(),
+                    Matches[4].str().c_str()))
+                {
+                    return TRUE;
+                }
+
+                HANDLE CurrentHandle = ::MoPrivateCreateFile(
+                    CurrentPath.c_str(),
+                    SYNCHRONIZE |
+                    FILE_LIST_DIRECTORY |
+                    DELETE |
+                    FILE_READ_ATTRIBUTES |
+                    FILE_WRITE_ATTRIBUTES,
+                    FILE_SHARE_READ | FILE_SHARE_WRITE,
+                    nullptr,
+                    OPEN_EXISTING,
+                    FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+                    nullptr);
+                if (CurrentHandle == INVALID_HANDLE_VALUE)
+                {
+                    ::MoPrivateWriteErrorMessage(
+                        Context,
+                        Mile::HResultFromLastError(FALSE),
+                        L"%s(%s)",
+                        L"CreateFileW",
+                        CurrentPath.c_str());
+                }
+
+                if (UsedSpace)
+                {
+                    UINT64 CurrentFileSize = 0;
+                    hr = Mile::GetCompressedFileSizeByHandle(
+                        CurrentHandle,
+                        &CurrentFileSize);
+                    if (hr.IsSucceeded())
+                    {
+                        *UsedSpace += CurrentFileSize;
+
+                        ::MoPrivateWriteLine(
+                            Context,
+                            L"Detected - %s.",
+                            CurrentPath.c_str());
+                    }
+                    else
+                    {
+                        ::MoPrivateWriteErrorMessage(
+                            Context,
+                            hr,
+                            L"%s(%s)",
+                            L"Mile::GetCompressedFileSizeByHandle",
+                            CurrentPath.c_str());
+                    }
+                }
+                else
+                {
+                    hr = Mile::DeleteFileByHandleIgnoreReadonlyAttribute(
+                        CurrentHandle);
+                    if (hr.IsSucceeded())
+                    {
+                        ::MoPrivateWriteLine(
+                            Context,
+                            L"Removed - %s.",
+                            CurrentPath.c_str());
+                    }
+                    else
+                    {
+                        ::MoPrivateWriteErrorMessage(
+                            Context,
+                            hr,
+                            L"%s(%s)",
+                            L"Mile::DeleteFileByHandleIgnoreReadonlyAttribute",
+                            CurrentPath.c_str());
+                    }
+                }
+
+                ::CloseHandle(CurrentHandle);
+
+                return TRUE;
+            });
+            if (hr.IsFailed())
+            {
+                ::MoPrivateWriteErrorMessage(
+                    Context,
+                    hr,
+                    L"%s(%s)",
+                    L"Mile::EnumerateFile",
+                    RootPath);
+            }
+
+            ::CloseHandle(RootHandle);
+        }
+        else
+        {
+            ::MoPrivateWriteErrorMessage(
+                Context,
+                Mile::HResultFromLastError(FALSE),
+                L"%s(%s)",
+                L"CreateFileW",
+                RootPath);
+        }
+    }
+}
 
 EXTERN_C HRESULT WINAPI MoPurgeNuGetCache(
     _In_ PNSUDO_CONTEXT Context)
@@ -19,6 +186,7 @@ EXTERN_C HRESULT WINAPI MoPurgeNuGetCache(
     Mile::HResult hr = S_OK;
     HANDLE PreviousContextTokenHandle = INVALID_HANDLE_VALUE;
     std::vector<std::wstring> CachePathList;
+    std::vector<std::wstring> PackagePathList;
 
     do
     {
@@ -74,10 +242,10 @@ EXTERN_C HRESULT WINAPI MoPurgeNuGetCache(
             Candidate = Profile + L"\\.nuget\\packages";
             if (::MoPrivateIsFileExist(Candidate.c_str()))
             {
-                CachePathList.push_back(Candidate);
+                PackagePathList.push_back(Candidate);
             }
         }
-        if (CachePathList.empty())
+        if (CachePathList.empty() && PackagePathList.empty())
         {
             hr = E_NOINTERFACE;
             ::MoPrivateWriteErrorMessage(
@@ -88,20 +256,39 @@ EXTERN_C HRESULT WINAPI MoPurgeNuGetCache(
         }
 
         UINT64 UsedSpace = 0;
+
         for (std::wstring const& CachePath : CachePathList)
         {
             if (PurgeMode == MO_PRIVATE_PURGE_MODE_SCAN)
             {
-                ::MoPrivateEmptyDirectoryWorker(
+                ::MoPrivateRemoveDirectoryWorker(
                     Context,
                     CachePath.c_str(),
                     &UsedSpace);
             }
             else if (PurgeMode == MO_PRIVATE_PURGE_MODE_PURGE)
             {
-                ::MoPrivateEmptyDirectoryWorker(
+                ::MoPrivateRemoveDirectoryWorker(
                     Context,
                     CachePath.c_str(),
+                    nullptr);
+            }
+        }
+
+        for (std::wstring const& PackagePath : PackagePathList)
+        {
+            if (PurgeMode == MO_PRIVATE_PURGE_MODE_SCAN)
+            {
+                ::PurgeNuGetPackageCacheWorker(
+                    Context,
+                    PackagePath.c_str(),
+                    &UsedSpace);
+            }
+            else if (PurgeMode == MO_PRIVATE_PURGE_MODE_PURGE)
+            {
+                ::PurgeNuGetPackageCacheWorker(
+                    Context,
+                    PackagePath.c_str(),
                     nullptr);
             }
         }
