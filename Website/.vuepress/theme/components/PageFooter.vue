0fa3cf75e0f42c641679b61fccee206f10da8062
qcyblm <qcyblm@139.com>
2021-09-05 15:59:54 +0800

    Update PageFooter.vue

--- Website/.vuepress/theme/components/PageFooter.vue
+++ Website/.vuepress/theme/components/PageFooter.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="page-footer">
+  <div class="page-footer" v-if='footer.actor'>
     <ul class="inner">
       <li v-if="footer.createUpdate">
         Copyright &copy; {{ footer.createYear }}-{{ new Date().getFullYear() }}
         <a v-if="footer.actorLink" :href="footer.actorLink" target="_blank" rel="noopener noreferrer"> {{ footer.actor }} </a> and Contributors. All rights reserved.
       </li>
-
       <li v-else-if="footer.createYear">
         Copyright &copy; {{ footer.createYear }} <a v-if="footer.actorLink" :href="footer.actorLink" target="_blank" rel="noopener noreferrer"> {{ footer.actor }} </a> and Contributors. All rights reserved.
       </li>
-      <li v-else>
+      <li v-else-if="footer.actorLink">
         Copyright &copy; <a v-if="footer.actorLink" :href="footer.actorLink" target="_blank" rel="noopener noreferrer"> {{ footer.actor }} </a> and Contributors. All rights reserved.
+      </li>
+      <li v-else-if="footer.actor">
+        Copyright &copy; {{ footer.actor }} and Contributors. All rights reserved.
+      </li>
+      <li v-if="footer.licensedLink">
+        <a :href="footer.licensedLink" target="_blank" rel="noopener noreferrer">{{ footer.licensed }}</a> Licensed
       </li>
       <li v-if="footer.licensedLink">
         <a :href="footer.licensedLink" target="_blank" rel="noopener noreferrer">{{ footer.licensed }}</a> Licensed
