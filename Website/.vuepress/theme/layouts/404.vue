0f9f8b9fd737be594c731c52e848bf3d7267dcf5
qcyblm <qcyblm@139.com>
2021-09-05 21:40:11 +0800

    add theme 404

---
+++ Website/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="theme-container">
+    <div class="theme-default-content">
+      <h1>404</h1>
+
+      <blockquote>{{ getMsg() }}</blockquote>
+
+      <a class="nav-link" href="/index.html">
+        Take me home.
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+const msgs = [
+  `There's nothing here.`,
+  `How did we get here?`,
+  `That's a Four-Oh-Four.`,
+  `Looks like we've got some broken links.`
+]
+
+export default {
+  methods: {
+    getMsg () {
+      return msgs[Math.floor(Math.random() * msgs.length)]
+    }
+  }
+}
+</script>
